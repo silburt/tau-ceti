4d0618bf8420207d2cc32b5d81c4d617cb4c819e
silburt <silburt@astro.utoronto.ca>
2018-04-18 15:05:13 -0400

    edit

--- plotting_PC_weights.py
+++ plotting_PC_weights.py
@@ -48,9 +48,24 @@ for lim in lims:
     plt.figure(figsize=(13,8))
     for i in range(0,n_draw):
         plt.plot(wavelengths, X[rs[i]], 'k,')
-    plt.plot(wavelengths, X_mean, linewidth=1, alpha=0.7)
+    plt.plot(wavelengths, X_mean, linewidth=1)
     plt.xlim([l1, l2])
     plt.xlabel('wavelength (angstroms)')
     plt.ylabel('flux')
     plt.savefig('output/mean_spectra_n%d_%d-%d.png'%(n_spec, l1, l2))
+    plt.close()
+
+lims = [(5000,5050),(6525,6600)]
+for lim in lims:
+    l1, l2 = lim
+    f, ax = plt.subplots(n_pcs, 1, sharex=True, figsize=(12,8))
+    plt.subplots_adjust(hspace=0)
+    ax[0].set_ylabel('PC 0 and 1')
+    ax[n_pcs-1].set_xlabel('wavelength (angstroms)')
+    for i in range(0, n_pcs):
+        ax[i].plot(wavelengths, V[i,:], ',')
+        ax[i].set_ylabel('PC %d'%(i+1))
+    ax[0].set_title('explained variance = %f, n_spectra=%d'%(ev, n_spec))
+    ax[0].set_xlim([l1,l2])
+    plt.savefig('output/pca_visualize_%dcomponents_n%d_%d-%d.png'%(n_pcs, n_spec, l1, l2))
     plt.close()
